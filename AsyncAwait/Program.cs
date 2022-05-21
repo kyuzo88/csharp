067f7916515043cba71a12f91a41b8c6cfa4cc9b
andi <meierand88@gmail.com>
2022-05-21 22:30:52 +0200

    Update Async

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Application started");
+
+var hello1 = WriteHelloAsync(1, 5);
+var hello2 = WriteHelloAsync(2, 3);
+var hello3 = WriteHelloAsync(3, 8);
+
+await Task.WhenAll(hello1, hello2, hello3);
+
+Console.WriteLine("Application finished");
+
+async Task WriteHelloAsync(int callOrder , int seconds)
+{
+    await Task.Delay(seconds *1000);
+    Console.WriteLine($"run {callOrder}:{seconds} seconds waited");
+}
